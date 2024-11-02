4c94ef9b8a1e1cf303f97804c63efde4a423e81f
PaigeYang <33799289+PaigeYang@users.noreply.github.com>
2024-11-01 23:42:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+from openai import OpenAI
+import os
 
-st.title("🎈 My new app")
+st.title("My IS883 Week7 Assignment")
+
+prompt = st.text_input("Share with us your experience of the latest trip")
+
+### Load your API Key
+os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
+
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+### OpenAI stuff
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": prompt}
+  ],
+)
+
+### Display
+st.write(
+    response.choices[0].message.content
 )
